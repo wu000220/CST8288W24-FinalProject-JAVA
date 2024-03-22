1d99472895286410a09f945010bec4d1233727ae
adaweideng <adaweideng@hotmail.com>
2024-03-22 16:41:18 -0400

    wei initial commit

---
+++ Final-Project/src/java/dataaccesslayer/DataSource.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dataaccesslayer;
+
+/**
+ *
+ * @author adawe
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+
+    private Connection connection = null;
+    private String url = "jdbc:mysql://localhost:3306/books?useSSL=false&allowPublicKeyRetrieval=true";
+    private String username = "root";
+    private String password = "1234";
+
+    public DataSource() {
+    }
+
+    /*
+ * Only use one connection for this application, prevent memory leaks.
+     */
+    public Connection createConnection() throws SQLException {
+        try {
+            if (connection != null) {
+                System.out.println("Cannot create new connection, one exists already");
+            } else {
+                connection = DriverManager.getConnection(url, username, password);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+        return connection;
+    }
+    
+    
+}
